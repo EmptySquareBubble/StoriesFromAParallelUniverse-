2b67c1df8442e54d9e2d151730de86f909e7b6ac
Jana Machutová <jana.machutova@gendigital.com>
2024-09-24 22:55:16 +0200

    partial_sum vs inclusive/exclusive_scan example

--- two_worlds.cpp
+++ two_worlds.cpp
@@ -4,10 +4,37 @@
 #include <execution>
 #include <iostream>
 #include <chrono>
+#include <numeric>
 
-constexpr int portion_size = 2;
+constexpr int portion_size = 1;
+constexpr int apetit_multiplier = 1;
 
-void balance_population(const std::vector<int>& species_population, const int factor)
+void portions_per_species(const std::vector<int>& species_population)
+{
+    //exclusive scan from partial_sum
+    //serial solution
+    std::vector<int> portions_sum = {0};
+    std::partial_sum(species_population.begin(), species_population.end(), std::back_inserter(portions_sum), [&portions_sum](const auto& prays_portions, const auto& prays_count){
+        return portions_sum[portions_sum.size()-1]/apetit_multiplier + prays_count/portion_size;
+        });
+    
+    std::cout << "portions avalilable for each species (partial_sum): ";
+    for(const auto& portion : portions_sum)
+        std::cout << portion << ",";
+    std::cout << "\n";
+
+    //parallelizable solution
+    std::vector<int> portions_scanned;
+    std::exclusive_scan(species_population.begin(), species_population.end(), std::back_inserter(portions_scanned), 0, [](const auto& prays_portions, const auto& prays_count){
+        return prays_portions/apetit_multiplier + prays_count/portion_size;});
+    
+    std::cout << "portions avalilable for each species (exclusive_scan): ";
+    for(const auto& portion : portions_scanned)
+        std::cout << portion << ",";
+    std::cout << "\n";
+}
+
+int next_generation_size(const std::vector<int>& species_population, const int factor)
 {
     //simple serial version
     auto next_gen_seq = std::accumulate(species_population.begin(), species_population.end(), 0, [factor](const auto first, const auto second){
@@ -20,6 +47,7 @@ void balance_population(const std::vector<int>& species_population, const int fa
         return item * factor;});
 
     //little bit more sophisticated calculation of next gen
+    //every spiecies wants to double its population, but also wants enough food for everyone 
     auto food_included = std::transform_reduce(std::execution::par, species_population.begin()+1, species_population.end(), species_population.begin(), species_population[0] * factor, std::plus{}, [factor](const auto& hunter, const auto& pray){
         const auto portions_available = pray / portion_size; 
         return std::clamp(portions_available, hunter, hunter * factor);});
@@ -28,6 +56,8 @@ void balance_population(const std::vector<int>& species_population, const int fa
     std::cout << "Population multiplied by factor " << factor << ":\n";
     std::cout << " seq: " << next_gen_seq << " reduce: " << next_gen_par << " transform_reduce: " << next_gen_par_fixed << std::endl;
     std::cout << "available food influences growth: " << food_included << std::endl;
+
+    return next_gen_par_fixed;
 }
 
 const auto list_of_species = std::ranges::iota_view{0, 1700};
@@ -59,7 +89,9 @@ int main()
     //build_underwater_kingdom();
 
     std::vector<int> species_chain{68, 15, 4, 45, 18, 3};
-    balance_population(species_chain, 2);
+    const auto creatures_cnt = next_generation_size(species_chain, 2);
+
+    portions_per_species(species_chain);
 
     return 0;
 }
