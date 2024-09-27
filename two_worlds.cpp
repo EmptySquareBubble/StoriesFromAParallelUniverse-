1fc2e8bb9fc84010aa8a4562f8d982de499a7d54
Jana Machutová <jana.machutova@gendigital.com>
2024-09-27 15:57:55 +0200

    food chain example to demonstrate partial_sum vs inclusive_scan and show small piping

--- two_worlds.cpp
+++ two_worlds.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <chrono>
 #include <numeric>
+#include <syncstream>
 
 constexpr int portion_size = 1;
 constexpr int apetit_multiplier = 1;
@@ -16,62 +17,63 @@ void dummy_expensive_computation(int seed)
         out += std::sqrt(i * std::rand());
 }
 
-void portions_per_species(const std::vector<int>& species_population)
+//count how much food is there available for each kind
+//everyone can eat their kind and smaller
+void edible_mass_per_species(const std::vector<int>& species_population, const std::vector<int>& species_weight)
 {
-    int weight_multiplier = 2;
-    //exclusive scan from partial_sum
+    int current_species_idx = 1;
+
     //serial solution
     std::vector<int> portions_sum(species_population.size());
-    std::partial_sum(species_population.begin(), species_population.end(), portions_sum.begin(), [&weight_multiplier](const auto& prays_portions, const auto& prays_count){
-        const auto out = prays_portions + prays_count * weight_multiplier;
-        weight_multiplier *= 2;
+    std::partial_sum(species_population.begin(), species_population.end(), portions_sum.begin(), [&current_species_idx, &species_weight](const auto& prays_portions, const auto& prays_count){
+        const auto out = prays_count * species_weight[current_species_idx++] + prays_portions;
         return out;
         });
     
-    std::cout << "portions avalilable for each species (partial_sum): ";
+    std::cout << "food amount avalilable for each species (partial_sum): ";
     for(const auto& portion : portions_sum)
         std::cout << portion << ",";
     std::cout << "\n";
 
     //parallelizable solution
-    weight_multiplier = 2;
+    //UB with other policy than std::execution::seq - concurrent acces to shared location current_species_idx - leaving here for better illustration 
+    //(with seq policy there is no difference from partial sum so we can't observe all problems)
+    current_species_idx = 1;
     std::vector<int> portions_scanned(species_population.size());
-    std::inclusive_scan(std::execution::par, species_population.begin(), species_population.end(), portions_scanned.begin(), [&weight_multiplier](const auto& prays_portions, const auto& prays_count){
+    std::inclusive_scan(std::execution::par, species_population.begin(), species_population.end(), portions_scanned.begin(), [&current_species_idx, &species_weight](const auto& prays_portions, const auto& prays_count){
         dummy_expensive_computation(prays_count);
-        const auto out = prays_portions + prays_count * weight_multiplier;
-        weight_multiplier *= 2;
-        return out;
+        return prays_count * species_weight[current_species_idx++] + prays_portions;
         });
     
-    std::cout << "portions avalilable for each species (inclusive_scan): ";
+    std::cout << "food amount avalilable for each species (inclusive_scan): ";
     for(const auto& portion : portions_scanned)
         std::cout << portion << ",";
     std::cout << "\n";
 
-    //fixed parallelizable solution
-    std::vector<int> weights(species_population.size());
-    int n = 1;
-    std::generate(weights.begin(), weights.end(), [&n]() { auto oldN = n; n *= 2; return oldN;});
-    std::vector<int> weighted_population(species_population.size());
-    std::transform(species_population.begin(), species_population.end(), weights.begin(), weighted_population.begin(), [](const auto& a, const auto& b){ return a * b;} );
-    std::cout << "transformed: \n";
-    for(const auto portion : weighted_population)
-        std::cout << portion << ", ";
-    std::cout << "\n";
-
-    std::cout << "inclusive scan: \n";
-    std::vector<int> portions_trans_scanned(species_population.size());
-    std::inclusive_scan(std::execution::par, weighted_population.begin(), weighted_population.end(), portions_trans_scanned.begin(), [&](const auto& prays_portions, const auto& prays_count){
+    //transform earlier
+    std::vector<int> weighed_population(species_population.size());
+    std::transform(species_population.begin(), species_population.end(), species_weight.begin(), weighed_population.begin(), [](const auto cnt, const auto w){return cnt * w;});
+    std::inclusive_scan(std::execution::par, weighed_population.begin(), weighed_population.end(), portions_scanned.begin(), [](const auto& prays_portions, const auto& prays_count){
         dummy_expensive_computation(prays_count);
-        return prays_portions + prays_count;
+        return prays_count + prays_portions;
         });
     
-    std::cout << "portions avalilable for each species (inclusive_scan after transform): ";
-    for(const auto& portion : portions_trans_scanned)
+    std::cout << "food amount  avalilable for each species (transform and inclusive_scan): ";
+    for(const auto& portion : portions_scanned)
         std::cout << portion << ",";
     std::cout << "\n";
 
-    //pipe it togehter
+    //pipe it nicely together
+    auto weighs = std::views::zip_transform(std::multiplies{}, species_population, species_weight);
+    std::inclusive_scan(std::execution::par, weighs.begin(), weighs.end(), portions_scanned.begin(), [](const auto& prays_portions, const auto& prays_count){
+        dummy_expensive_computation(prays_count);
+        return prays_count + prays_portions;
+        });
+    
+    std::cout << "food amount  avalilable for each species (transform piped insice inclusive_scan): ";
+    for(const auto& portion : portions_scanned)
+        std::cout << portion << ",";
+    std::cout << "\n";
 }
 
 int next_generation_size(const std::vector<int>& species_population, const int factor)
@@ -80,10 +82,10 @@ int next_generation_size(const std::vector<int>& species_population, const int f
     auto next_gen_seq = std::accumulate(species_population.begin(), species_population.end(), 0, [factor](const auto first, const auto second){
         return first + second * factor;});
     //not processed in order! wrong result
-    auto next_gen_par = std::reduce(species_population.begin(), species_population.end(), 0, [factor](const auto first, const auto second){
+    auto next_gen_par = std::reduce(std::execution::par, species_population.begin(), species_population.end(), 0, [factor](const auto first, const auto second){
         return first + second * factor;});
     //fix it it with transform reduce
-    auto next_gen_par_fixed = std::transform_reduce(species_population.begin(), species_population.end(), 0, std::plus{}, [factor](const auto item){
+    auto next_gen_par_fixed = std::transform_reduce(std::execution::par, species_population.begin(), species_population.end(), 0, std::plus{}, [factor](const auto item){
         return item * factor;});
 
     //little bit more sophisticated calculation of next gen
@@ -125,13 +127,22 @@ void build_underwater_kingdom()
 
 int main()
 {
+    //two different world made using two different approach - which one is better and why
     //build_sky_empire();
     //build_underwater_kingdom();
 
-    std::vector<int> species_chain{68, 15, 4, 45, 18, 3};
-    const auto creatures_cnt = next_generation_size(species_chain, 2);
+    //our world has nice natural chain - here is listed size of population of each of the species ordered from the smallest/weakest prays to the biggest predators
+    std::vector<int> species_chain{68, 15, 4, 45, 18, 3, 2, 11};
 
-    portions_per_species(species_chain);
+    //how will the total population size change in the next generation if we will expect growt by given factor
+    constexpr int factor = 2;
+    const auto creatures_cnt = next_generation_size(species_chain, factor);
+
+    //make up list of weights for each kind. increasing row as probably larger are predators and smller prays
+    //extra 1 at the end to make this example run even when i leave there nasty behaviour in first paralelizaion attempt
+    const std::vector<int> species_weight{1, 2, 3, 4, 5, 6, 7, 8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    //find out food options per species
+    edible_mass_per_species(species_chain, species_weight);
 
     return 0;
 }
