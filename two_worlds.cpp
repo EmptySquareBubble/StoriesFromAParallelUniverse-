20873426b47e6d3852981afdc13f440370993416
Jana Machutová <jana.machutova@gendigital.com>
2024-10-22 09:29:16 +0200

    cosmetic changes

--- two_worlds.cpp
+++ two_worlds.cpp
@@ -27,7 +27,7 @@ void edible_mass_avoid_sync(const std::vector<int>& species_population, const st
     std::print("\n piped into exclusive_scan: ");
     for(const auto& portion : portions_scanned)
         std::print("{}g, ", portion);
-    std::print("\n");
+    std::print("\n\n");
 }
 
 void edible_mass_no_canibalism(const std::vector<int>& species_mass, const int growth_factor)
@@ -86,10 +86,10 @@ void edible_mass_per_species(const std::vector<int>& species_mass, const int gro
 		return equal_pray * growth_factor + smaller_pray;
     });
 
-    std::print("g\n naive inclusive_scan: ");
+    std::print("\n naive inclusive_scan: ");
     for(const auto& portion : edible_mass)
-        std::print("{}, ", portion);
-    std::print("g\n");
+        std::print("{}g, ", portion);
+    std::print("\n");
 }
 
 void total_mass(const std::vector<int>& species_population, const std::vector<float>& species_weight)
@@ -125,9 +125,7 @@ void next_generation_size(const std::vector<int>& species_population, const int 
     std::print(" seq: {} residents\n reduce: {} residents\n naive reduce: {} residents\n transform_reduce: {} residents\n naive transform_reduce {} residents\n", next_gen_seq, next_gen_par, next_gen_par_naive, next_gen_par_fixed, next_gen_par_naive_fixed);
 }
 
-const auto list_of_species = std::ranges::iota_view{0, 1700};
-
-void build_sky_empire()
+void build_sky_empire(const auto& list_of_species)
 {
     std::vector<std::jthread> creatures;
 
@@ -140,7 +138,7 @@ void build_sky_empire()
 
 }
 
-void build_underwater_kingdom()
+void build_underwater_kingdom(const auto& list_of_species)
 {
     //doesn't really make that much threads has clever management under the hood!
     std::for_each(std::execution::par, list_of_species.begin(), list_of_species.end(), [&](const auto id){
@@ -186,15 +184,17 @@ void fish_and_shark()
     ///comparison of different execution policies
     std::vector<Fish> fishes{{32, 10}, {54, 15}, {55, 5}, {123, 20}, {124, 18}, {124, 18}, {320, 2}, {323, 5}, {480, 16}};
     std::array<Hideout, 7> hideouts{Hideout{30, 1}, Hideout{54, 1}, Hideout{123, 1}, Hideout{145, 1}, Hideout{345, 1}, Hideout{423, 1}, Hideout{700, 1}};
-    auto time_interval = 1;
+    const auto time_interval = 1;
     std::atomic<bool> shark = true;
     
+    std::print("Beware of shark!\n");
+                
     //ok for par once shark is atomic or locked
     std::for_each(std::execution::par, fishes.begin(), fishes.end(), [&](auto& fish)
     {
         if(!shark)
         {
-            fish.pos = fish.pos + time_interval * fish.velocity;
+            fish.pos += time_interval * fish.velocity;
         }
         else
         {
@@ -211,15 +211,15 @@ void fish_and_shark()
 
             if(nearest_hideouts == free_hideouts.end())
             {
-                std::print("{} don't have any free hidout\n", fish.pos);
+                std::print(" fish at pos {} doesn't have any free hidout nearby\n", fish.pos);
                 return;
             }
+
             const auto first = nearest_hideouts;
             const auto second = ++nearest_hideouts;
-
-            const auto txt_begin = std::format("{} goes for: [{}, {}] -> ", fish.pos, first->pos, (second == free_hideouts.end()) ? 0 : second->pos);
             
             auto& selected_hideout = fish.pos < std::midpoint(first->pos, second->pos) ? first : second;   
+            const auto txt_begin = std::format(" fisht at pos {} runs to hideout at {}", fish.pos, selected_hideout->pos);
             
             //looong swim to hideout (to be able to see effect of spot stealing)
             const auto distance = std::abs(selected_hideout->pos - fish.pos);
@@ -228,20 +228,20 @@ void fish_and_shark()
             if(selected_hideout->take_slot())
             {
                 fish.pos = selected_hideout->pos;
-                std::print("{}{}\n", txt_begin, fish.pos);
+                std::print("{} succesfully!\n", txt_begin);
             }
             else
-                std::print("{}was too slow - hideout is full\n", txt_begin);     
+                std::print("{} and arrives too late - hideout is full\n", txt_begin);     
         }
     });
 
-    std::print("fishes final positions: \n");
+    std::print("\n fishes final positions: \n  ");
     for(auto fish : fishes)
     {
         std::print("{}, ", fish.pos);
     }
 
-    std::print("\nhideouts final state: \n");
+    std::print("\n hideouts final state: \n  ");
     for(auto& hideout : hideouts)
     {
         std::print("[{}, {}]", hideout.pos, hideout.slots_cnt());
@@ -256,35 +256,47 @@ int main()
     manual threads will drain your cpu while paralel alg version will stay sustainable thanks to thread management hidden behind
     commented out to make initial run of this example smooth - feel free to uncomment, but expect intensive load for your CPU
     */
-    //build_sky_empire();
-    //build_underwater_kingdom();
+    /*
+    std::print("Create sky and underwater worlds\n");
+    const auto list_of_species = std::ranges::iota_view{0, 1700};
+    build_sky_empire(list_of_species);
+    build_underwater_kingdom(list_of_species);
+    */
 
     //our world has nice natural chain - here is listed size of population of each of the species ordered from the smallest/weakest prays to the biggest predators
     std::vector<int> species_chain{68, 15, 4, 45, 18, 3, 2, 11};
-
     //how will the total population size change in the next generation if we will expect growth by given factor
     constexpr int factor = 2;
+
+    //get number of animals alive in next generation with given growth factor
     next_generation_size(species_chain, factor);
 
     //make up list of weights for each kind. increasing row as probably larger are predators and smaller prays
     const std::vector<float> species_weight{1e-6f, 2.1e-5f, 3.f, 43.3f, 5.1e3f, 6.5e4f, 7.7e6f, 1.5e8f};
+    //get total mass of animals alive
     total_mass(species_chain, species_weight);
     
-    //find out food options per species
-    std::vector<int> species_mass;
+    //simplify weights to not deal with floating point values
+    //smallest are of weight 1 next 2 etc..
+    std::vector<int> species_mass;  //total mass per species
     std::ranges::transform(std::ranges::iota_view{1, 9}, species_chain, std::back_inserter(species_mass), std::multiplies{});
+    //how much food will be available for each species in next generation with given growth factor - canibalism enabled
     edible_mass_per_species(species_mass, factor);
 
+    //how much food will be available for each species in next generation with given growth factor - canibalism not enabled
     edible_mass_no_canibalism(species_mass, factor);
     
     //put multiple operations into one smooth chain without any sync between each step
     edible_mass_avoid_sync(species_chain, std::ranges::iota_view{1, 9} | std::ranges::to<std::vector>(), factor);
 
-    //demonstrate different execution policies in one example 
+    //demonstrate different execution policies in one example
+    //everyone is floating in given speed peacefully until shark attacks
+    //then everyone looks for the closest hideout to stay safe 
     fish_and_shark();
+
     /*todo: 
-    for_each a for_each_n je taky novy
-    transform_exclusive_scan + transform_inclusive_scan taky existuje
+    for_each a for_each_n je taky novy v 17
+    transform_exclusive_scan + transform_inclusive_scan taky existuje (transform_cokoli_noveho)
     */
 
     return 0;
